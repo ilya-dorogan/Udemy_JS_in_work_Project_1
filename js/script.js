5b6a9018926f4b11ff7c1662b096f4e2c34d102c
Ilya Dorogan <enotshannel@gmail.com>
2020-08-16 18:20:01 +0300

    modal_windows_update

--- js/script.js
+++ js/script.js
@@ -98,18 +98,28 @@ window.addEventListener('DOMContentLoaded', () => {
 		modal = document.querySelector('.modal'),
 		modalCloseBtn = document.querySelector('[data-close]');
 
+	function openModal() {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
+
 	function closeModal() {
 		modal.classList.add('hide');
 		modal.classList.remove('show');
 		document.body.style.overflow = '';
 	}
 
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll)
+		}
+	}
+
 	modalTrigger.forEach(btn => {
-		btn.addEventListener('click', () => {
-			modal.classList.add('show');
-			modal.classList.remove('hide');
-			document.body.style.overflow = 'hidden';
-		});
+		btn.addEventListener('click', openModal);
 	});
 
 	modalCloseBtn.addEventListener('click', closeModal);
@@ -125,5 +135,9 @@ window.addEventListener('DOMContentLoaded', () => {
 			closeModal();
 		}
 	});
+
+	const modalTimerId = setTimeout(openModal, 2000);
+
+	window.addEventListener('scroll', showModalByScroll);
 
 });
